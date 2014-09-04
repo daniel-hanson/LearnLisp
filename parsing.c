af73568d9ef140c58f1f4910d51fdee29d1bdea3
daniel-hanson <dhansonmu@gmail.com>
2014-09-03 21:27:33 -0500

    Tweaked a few of the interface components

--- parsing.c
+++ parsing.c
@@ -7,10 +7,10 @@ static char buffer[2048];
 char* readline(char* prompt) {
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
-  return cpy;
+  char* copy = malloc(strlen(buffer)+1);
+  strcpy(copy, buffer);
+  copy[strlen(copy)-1] = '\0';
+  return copy;
 }
 
 void add_history(char* unused) {}
@@ -24,13 +24,13 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
   
-  /* Create Some Parsers */
+  // Create Some Parsers
   mpc_parser_t* Number   = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lispy    = mpc_new("lispy");
   
-  /* Define them with the following Language */
+  // Define them with the following Language
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
       number   : /-?[0-9]+/ ;                             \
@@ -40,30 +40,30 @@ int main(int argc, char** argv) {
     ",
     Number, Operator, Expr, Lispy);
   
-  puts("Lispy Version 0.0.0.0.2");
-  puts("Press Ctrl+c to Exit\n");
+  puts("LearnLisp Version 0.1");
+  puts("Press Ctrl+C to Exit\n");
   
   while (1) {
   
-    char* input = readline("lispy> ");
+    char* input = readline("LearnLisp> ");
     add_history(input);
     
-    /* Attempt to parse the user input */
-    mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      /* On success print and delete the AST */
-      mpc_ast_print(r.output);
-      mpc_ast_delete(r.output);
+    // Attempt to parse the user input
+    mpc_result_t result;
+    if (mpc_parse("<stdin>", input, Lispy, &result)) {
+      // On success print and delete the AST
+      mpc_ast_print(result.output);
+      mpc_ast_delete(result.output);
     } else {
-      /* Otherwise print and delete the Error */
-      mpc_err_print(r.error);
-      mpc_err_delete(r.error);
+      // Otherwise print and delete the Error
+      mpc_err_print(result.error);
+      mpc_err_delete(result.error);
     }
     
     free(input);
   }
   
-  /* Undefine and delete our parsers */
+  // Undefine and delete our parsers
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
   
   return 0;
